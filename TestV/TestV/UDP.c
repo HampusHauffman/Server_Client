637ede20842431221d43ccd7d786e1d8c71b8d4d
Hampus <hampus.hauffman@gmail.com>
2017-05-08 12:41:48 +0200

    minor tweeks
    Totally works hrer

--- TestV/TestV/UDP.c
+++ TestV/TestV/UDP.c
@@ -1,6 +1,7 @@
 //----------------------------------------------------------------------
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //----------------------------------------------------------------------
 #define SDL_MAIN_HANDLED
@@ -13,21 +14,30 @@
 int UDP()
 {
 	printf("UDP\n");
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDLNet_Init();
+
+
+	IPaddress adrs[4] = { 0,0,0,0 };
+
 
 	IPaddress ip;
 	UDPsocket server;
 	UDPsocket client;
 	UDPpacket *packet;
+	UDPpacket *packets;
 
 	server = SDLNet_UDP_Open(1234);
+	client = SDLNet_UDP_Open(0);
 
 	packet = SDLNet_AllocPacket(1024);
+	packets = SDLNet_AllocPacket(1024);
+
+	
+
 
 	while (1)
 	{
 		/* Wait a packet. UDP_Recv returns != 0 if a packet is coming */
+		SDL_Delay(1000);
 		while (SDLNet_UDP_Recv(server, packet))
 		{
 			printf("UDP Packet incoming\n");
@@ -37,6 +47,16 @@ int UDP()
 			printf("\tMaxlen:  %d\n", packet->maxlen);
 			printf("\tStatus:  %d\n", packet->status);
 			printf("\tAddress: %x %x\n", packet->address.host, packet->address.port);
+
+			//detta skickar med ipadressen som kom
+			packets->address.host = packet->address.host;	
+			packets->address.port = packet->address.port;
+
+			packets->data = "da";
+			packets->len = strlen((char *)packets->data) + 1;
+
+			SDLNet_UDP_Send(client, -1, packets); /* This sets the p->channel */
+
 
 		}
 	}
