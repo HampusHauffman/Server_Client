0f717e422449257cf241327edceecf874309206f
Hampus <hampus.hauffman@gmail.com>
2017-05-09 20:45:52 +0200

    udp changes

--- TestV/TestV/UDP.c
+++ TestV/TestV/UDP.c
@@ -16,8 +16,13 @@ int UDP()
 	printf("UDP\n");
 
 
-	IPaddress adrs[4] = { 0,0,0,0 };
-
+	IPaddress adrs[4];
+	int i = 0;
+	int k = 0;
+	adrs[0].port = 0;
+	adrs[1].port = 0;
+	adrs[2].port = 0;
+	adrs[3].port = 0;
 
 	IPaddress ip;
 	UDPsocket server;
@@ -32,13 +37,14 @@ int UDP()
 	packets = SDLNet_AllocPacket(1024);
 
 	
+	//printf("%x", adrs[0]);
 
 
 	while (1)
 	{
 		/* Wait a packet. UDP_Recv returns != 0 if a packet is coming */
-		SDL_Delay(1000);
-		while (SDLNet_UDP_Recv(server, packet))
+		//SDL_Delay(1000);
+		if(SDLNet_UDP_Recv(server, packet))
 		{
 			printf("UDP Packet incoming\n");
 			printf("\tChan:    %d\n", packet->channel);
@@ -47,17 +53,29 @@ int UDP()
 			printf("\tMaxlen:  %d\n", packet->maxlen);
 			printf("\tStatus:  %d\n", packet->status);
 			printf("\tAddress: %x %x\n", packet->address.host, packet->address.port);
+			
+			
+				if((adrs[i].port == 0x00) && (packet->address.port != adrs[0].port) && (packet->address.port != adrs[1].port) && (packet->address.port != adrs[2].port) && (packet->address.port != adrs[3].port))
+				{
+					adrs[i] = packet->address;
+					printf("Saved port: %x to %d\n", adrs[i].port,i);
+					i++;
+				}
+			
 
 			//detta skickar med ipadressen som kom
 			packets->address.host = packet->address.host;	
 			packets->address.port = packet->address.port;
+			//scanf(&packet->data);
 
-			packets->data = "da";
+			
 			packets->len = strlen((char *)packets->data) + 1;
 
 			SDLNet_UDP_Send(client, -1, packets); /* This sets the p->channel */
 
-
+			if (i == 4) {
+				i = 0;
+			}
 		}
 	}
 
