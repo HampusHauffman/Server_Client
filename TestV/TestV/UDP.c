cd65d3e9ba4eada593eac407cb44fca509872d3e
HampusHauffman <hampus.hauffman@gmail.com>
2017-05-11 23:05:00 +0200

    multiple client support hopefully

--- TestV/TestV/UDP.c
+++ TestV/TestV/UDP.c
@@ -48,6 +48,7 @@ int UDP()
 		//SDL_Delay(1000);
 		if(SDLNet_UDP_Recv(server, packet))
 		{
+			/*
 			printf("UDP Packet incoming\n");
 			printf("\tChan:    %d\n", packet->channel);
 			printf("\tData:    %s\n", (char *)packet->data);
@@ -55,30 +56,63 @@ int UDP()
 			printf("\tMaxlen:  %d\n", packet->maxlen);
 			printf("\tStatus:  %d\n", packet->status);
 			printf("\tAddress: %x %x\n", packet->address.host, packet->address.port);
+			*/
+			
+			
 
-			
-			
-			sscanf(packet->data, "%d %d %d", &objOne[0], &objOne[1], &objOne[2]);
-			printf("\n%d \n%d \n%d\n", objOne[0], objOne[1], objOne[2]);
-			
+				//gives each adress a unique place in adrs
 				if((adrs[i].port == 0x00) && (packet->address.port != adrs[0].port) && (packet->address.port != adrs[1].port) && (packet->address.port != adrs[2].port) && (packet->address.port != adrs[3].port))
 				{
 					adrs[i] = packet->address;
 					printf("Saved port: %x to %d\n", adrs[i].port,i);
 					i++;
 				}
-			
+				if (i == 0) {
+					printf("nothing has connected yet");
+				}
+				if(i == 1){//One
+					sscanf(packet->data, "%d %d %d", &objOne[0], &objOne[1], &objOne[2]);
+					printf("\nData Recieved: %d %d %d\n", objOne[0], objOne[1], objOne[2]);
+				}
+				if (i == 2) {//Two
+					sscanf(packet->data, "%d %d %d", &objTwo[0], &objTwo[1], &objTwo[2]);
+					printf("\nData Recieved: %d %d %d\n", objTwo[0], objTwo[1], objTwo[2]);
+				}
+
+				if (i == 3) {//Three
+					sscanf(packet->data, "%d %d %d", &objThree[0], &objThree[1], &objThree[2]);
+					printf("\nData Recieved: %d %d %d\n", objThree[0], objThree[1], objThree[2]);
+				}
+
+				if (i == 4) {//Four
+					sscanf(packet->data, "%d %d %d", &objFour[0], &objFour[1], &objFour[2]);
+					printf("\nData Recieved: %d %d %d\n", objFour[0], objFour[1], objFour[2]);
+				}
+
+
 			/*
 			
 			Här kan man ändra på data som ska skickas tillbaka
 			
 			*/
 			//tex 
-				objOne[0] = objOne[0] + 1;
-
-			sprintf(hold, "%d %d %d", (double)objOne[0], (double)objOne[1], (double)objOne[2]);
-			packets->data = hold;
-
+			//objOne[0] = objOne[0] + 1;
+				if (i == 1) {//One
+					sprintf(hold, "%d %d %d", (double)objOne[0], (double)objOne[1], (double)objOne[2]);
+					packets->data = hold;
+				}
+				if (i == 2) {//Two
+					sprintf(hold, "%d %d %d", (double)objTwo[0], (double)objTwo[1], (double)objTwo[2]);
+					packets->data = hold;
+				}
+				if (i == 3) {//Three
+					sprintf(hold, "%d %d %d", (double)objThree[0], (double)objThree[1], (double)objThree[2]);
+					packets->data = hold;
+				}
+				if (i == 4) {//Four
+					sprintf(hold, "%d %d %d", (double)objFour[0], (double)objFour[1], (double)objFour[2]);
+					packets->data = hold;
+				}
 			//detta skickar med ipadressen som kom
 			packets->address.host = packet->address.host;	
 			packets->address.port = packet->address.port;
@@ -88,10 +122,10 @@ int UDP()
 			packets->len = strlen((char *)packets->data) + 1;
 
 			SDLNet_UDP_Send(client, -1, packets); /* This sets the p->channel */
-
+			/* not necesarry since we never use more than 5 players
 			if (i == 4) {
 				i = 0;
-			}
+			}*/
 		}
 	}
 
